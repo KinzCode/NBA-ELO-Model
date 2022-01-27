281a5311aa82da633daaf975887214d32daddbea
KinzCode <47777487+KinzCode@users.noreply.github.com>
2022-01-27 11:20:37 +1100

    added doc string to map_team_names and merge_data functions

--- src/merge.py
+++ src/merge.py
@@ -8,6 +8,21 @@ Created on Tue Jan 25 14:02:18 2022
 import pandas as pd
 
 def map_team_names(games, teams):
+    """
+    Parameters
+    ----------
+    games : DF
+        The df where each row represents a unique NBA game. DF contains attributes
+        such as date, home/visitor team ID, scores etc.
+    teams : DF
+        The df containing the team ID and the respetive information associated to
+        that ID. This includes : city, mascot etc.
+    Returns
+    -------
+    mapped_games : DF
+        The games df with the respective team names mapped to the team ID.
+
+    """
     teams['TEAM_NAME'] = teams['CITY'] + " " + teams['NICKNAME']
     team_id_dict = dict(zip(teams['TEAM_ID'], teams['TEAM_NAME']))
     games['HOME_TEAM_ID'] = games['HOME_TEAM_ID'].map(team_id_dict)
@@ -15,6 +30,22 @@ def map_team_names(games, teams):
     return games
 
 def merge_data(games, teams):
+    """
+    Parameters
+    ----------
+    games : DF
+        The df where each row represents a unique NBA game. DF contains attributes
+        such as date, home/visitor team ID, scores etc.
+    teams : DF
+        The df containing the team ID and the respetive information associated to
+        that ID. This includes : city, mascot etc.
+
+    Returns
+    -------
+    mapped_games : DF
+        The games df with the respective team names mapped to the team ID.
+
+    """
     mapped_games = map_team_names(games, teams)
     return mapped_games
 
